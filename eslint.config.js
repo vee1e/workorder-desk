99458eb56255db17f25a9bbf90b3834e84a77319
lakshit verma <vermalucky2004@gmail.com>
2026-08-17 01:31:48 +0530

    chore(monorepo): scaffold npm workspaces and tooling

---
+++ eslint.config.js
@@ -0,0 +1,15 @@
+const js = require('@eslint/js');
+const tseslint = require('typescript-eslint');
+const prettier = require('eslint-config-prettier');
+
+module.exports = tseslint.config(
+  { ignores: ['**/dist/**', '**/node_modules/**', '**/coverage/**'] },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+    },
+  },
+);
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  semi: true,
+  tabWidth: 2,
+};
